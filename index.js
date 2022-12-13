ab296ee5b87b588fa7a17d80b1c575ef349b7364
Bidisha12 <bidishapaul237@gmail.com>
2022-12-13 13:15:18 +0530

    ready_assist assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function addChallange() {
+    // Add challange logic
+    console.log('Add challange');
+}
+
+function takeChallange() {
+    // Take challange logic
+    console.log('Take challange');
+}
+
+function playNow() {
+    // Play now logic
+    console.log('Play now');
+}
